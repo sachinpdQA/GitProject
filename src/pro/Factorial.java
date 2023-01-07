65542fcfe530b92e8f974b14c526974f22208012
Sachin <Sachin@Sachin>
2023-01-07 09:50:00 +0530

    First commit....

---
+++ src/pro/Factorial.java
@@ -0,0 +1,16 @@
+package pro;
+
+public class Factorial {
+	public static void main(String[] args)
+	{
+		double a=1,b=15;    //result of factorial is always greater
+		
+		for(int i=1;i<=b;i++)
+		{
+			a=a*i;          
+		}
+		System.out.println("Factorial of "+b+"! ="+a);  
+		// in o/p E represent exponential
+	}
+
+}
